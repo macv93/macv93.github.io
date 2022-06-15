90bb49489dbf90b13deecb3c2f4d45e046f97530
macv93 <m.canarte@vehikl.com>
2022-06-15 17:21:51 -0400

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import rinnegan from "./rinnegan.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img src={rinnegan} className="App-logo" alt="logo" />
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://manuelcanarte.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Manny Cañarte
         </a>
       </header>
     </div>
