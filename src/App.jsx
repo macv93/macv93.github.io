5562b04438f6ddad8139f5a61c066044a15b3ad8
manny <manuel.canarte@gmail.com>
2025-02-27 11:37:54 -0500

    Add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -65,17 +65,12 @@ const App = () => {
       >
         <div style={{ padding: "1rem" }}>Manny</div>
         <ul style={{ display: "flex", gap: "1rem" }}>
-          <a href={LINKEDIN_URL} title="Connect with me on LinkedIn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.26c-.97 0-1.75-.79-1.75-1.76s.78-1.76 1.75-1.76 1.75.79 1.75 1.76-.78 1.76-1.75 1.76zm13.5 12.26h-3v-5.6c0-3.36-4-3.11-4 0v5.6h-3v-11h3v1.76c1.39-2.58 7-2.77 7 2.47v6.77z" />
-            </svg>
+          <a href={GITHUB_URL} title="Connect with me on LinkedIn">
+            GitHub
           </a>
-
+          <a href={LINKEDIN_URL} title="Connect with me on LinkedIn">
+            LinkedIn
+          </a>
           <a href="mailto:manuel.canarte@gmail.com" title="Let's connect!">
             Contact
           </a>
