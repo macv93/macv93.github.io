f7372045041659d0374159b9c5396d19d87b6d48
manny <manuel.canarte@gmail.com>
2025-03-12 14:08:30 -0400

    Add more photos and rename extensions

--- src/components/helpers/images.js
+++ src/components/helpers/images.js
@@ -1,12 +1,39 @@
-import gamingRig from "/images/the-rig.jpg"
-import bc1 from "/images/bc1.jpg"
-import bc2 from "/images/bc2.jpg"
-import costaRica from "/images/costa-rica.jpg"
-import dallas from "/images/dallas.jpg"
-import frog from "/images/frog.jpg"
-import nissan from "/images/mexico-nissan.jpg"
-import nashville from "/images/nashville.jpg"
-import turtles from "/images/turtles.jpg"
-import toronto from "/images/toronto.jpg"
+import gamingRig from "/images/the-rig.jpeg";
+import bc1 from "/images/bc1.jpeg";
+import bc2 from "/images/bc2.jpeg";
+import costaRica from "/images/costa-rica.jpeg";
+import dallas from "/images/dallas.jpeg";
+import frog from "/images/frog.jpeg";
+import nissan from "/images/mexico-nissan.jpeg";
+import nashville from "/images/nashville.jpeg";
+import turtles from "/images/turtles.jpeg";
+import toronto from "/images/toronto.jpeg";
+import fish from "/images/black-fish.jpeg";
+import tokyo from "/images/tokyo.jpeg";
+import tokyo2 from "/images/tokyo2.jpeg";
+import tokyo3 from "/images/tokyo3.jpeg";
+import tokyo4 from "/images/tokyo4.jpeg";
+import skyline from "/images/skyline.jpeg";
+import samba from "/images/samba-fest.jpeg";
+import asakusa from "/images/asakusa.jpeg";
 
-export const allImages = [gamingRig, toronto, nissan, bc1, frog, bc2, costaRica, dallas, turtles, nashville, ]
+export const allImages = [
+  gamingRig,
+  samba,
+  tokyo4,
+  fish,
+  toronto,
+  tokyo,
+  nissan,
+  tokyo2,
+  skyline,
+  bc1,
+  tokyo3,
+  frog,
+  bc2,
+  costaRica,
+  dallas,
+  asakusa,
+  turtles,
+  nashville,
+];
