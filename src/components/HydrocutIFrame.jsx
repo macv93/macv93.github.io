082e6163ca465327446dffb96cc0de4d52c3bac7
manny <manuel.canarte@gmail.com>
2025-03-17 17:26:08 -0400

    Update iframe dimensions

--- src/components/HydrocutIFrame.jsx
+++ src/components/HydrocutIFrame.jsx
@@ -2,9 +2,10 @@ const HydrocutIFrame = () => {
   return (
     <iframe
       className="p-4 pr-4 max-w-lg"
+      height={315}
+      width={560}
       src="https://www.youtube.com/embed/k6_N9JR1A_I?si=fIqaU8y2CIqQ6ZVz&amp;start=45"
       title="YouTube video player"
-      frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       referrerPolicy="strict-origin-when-cross-origin"
       allowFullScreen={true}
