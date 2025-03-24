e7854b0043afd8dae70466e504e20e44d7a5cb1c
Manny Cana <manuel.canarte@gmail.com>
2025-03-23 23:34:41 -0400

    Add scale duration to thumbnails

--- src/components/ImagePreviewRow.tsx
+++ src/components/ImagePreviewRow.tsx
@@ -55,7 +55,7 @@ const ImagePreviewRow = () => {
                   key={imageIndex}
                   src={image.thumbnail}
                   alt={`Loading...`}
-                  className="w-20 h-20 m-1 cursor-pointer object-cover hover:scale-110 rounded-md"
+                  className="w-20 h-20 m-1 cursor-pointer object-cover duration-300 hover:scale-110 rounded-md"
                   onClick={(e) => {
                     e.stopPropagation();
                     handleImageClick(image);
